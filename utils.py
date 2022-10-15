787dac2c5f66ed5fa7d5314f035ed7820f739ab1
Yewen Zhou <yewenzhou@berkeley.edu>
2022-10-15 16:20:25 -0400

    Update EDA_yewen and utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
 from skimage import io
+import numpy as np
 
 NLCD_CLASS = {
+     0: "No Data",
     11: "Open Water",
     12: "Perennial Ice/Snow",
     21: "Developed, Open Space",
@@ -13,17 +15,42 @@ NLCD_CLASS = {
     41: "Deciduous Forest",
     42: "Evergreen Forest",
     43: "Mixed Forest",
-    51: "Dwarf Scrub",
     52: "Shrub/Scrub",
     71: "Grassland/Herbaceous",
-    72: "Sedge/Herbaceous",
-    73: "Lichens",
-    74: "Moss",
     81: "Pasture/Hay",
     82: "Cultivated Crops",
     90: "Woody Wetlands",
     95: "Emergent Herbaceous Wetlands"
 }
+
+MAPPING = {
+    "Water": [11],
+    "Tree Canopy": [41, 42, 43, 52, 90, 95],
+    "Low Vegetation": [21, 71, 81, 82],
+    "Impervious": [22, 23, 24, 31],
+    "No Data": [0, 12],
+}
+
+
+NLCD_IDX_TO_REDUCED_LC_MAP = np.array([
+    4,#  0 No data 0
+    0,#  1 Open Water
+    4,#  2 Ice/Snow
+    2,#  3 Developed Open Space
+    3,#  4 Developed Low Intensity
+    3,#  5 Developed Medium Intensity
+    3,#  6 Developed High Intensity
+    3,#  7 Barren Land
+    1,#  8 Deciduous Forest
+    1,#  9 Evergreen Forest
+    1,# 10 Mixed Forest
+    1,# 11 Shrub/Scrub
+    2,# 12 Grassland/Herbaceous
+    2,# 13 Pasture/Hay
+    2,# 14 Cultivated Crops
+    1,# 15 Woody Wetlands
+    1,# 16 Emergent Herbaceious Wetlands
+])
 
 
 def get_image_ndarray(csv_path, col, row):
