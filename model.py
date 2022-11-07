181756fb14582874aeeb5427a0c89a6105eddecd
Yewen <yewenzhou@berkeley.edu>
2022-11-07 11:39:13 -0500

    Update model

--- model.py
+++ model.py
@@ -5,10 +5,10 @@ from keras.layers import Conv2D, BatchNormalization
 def FCN(input_size, classes):
     """ref: https://colab.research.google.com/drive/1VgwnN7jf-eZ12BaoyiuIKrqeWWprU7Nq?usp=sharing"""
     inputs = keras.Input(shape=(input_size, input_size, 4))
-    x = Conv2D(filters=64, kernel_size=3, strides=1, padding="same", activation="gelu")(inputs)
+    x = Conv2D(filters=16, kernel_size=3, strides=1, padding="same", activation="gelu")(inputs)
     x = BatchNormalization()(x)
     
-    x = Conv2D(filters=32, kernel_size=3, strides=1, padding="same", activation="relu")(x)
+    x = Conv2D(filters=16, kernel_size=3, strides=1, padding="same", activation="relu")(x)
     x = BatchNormalization()(x)
     
     x = Conv2D(filters=16, kernel_size=3, strides=1, padding="same", activation="relu")(x)
